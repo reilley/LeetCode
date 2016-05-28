03ad9db87e0913dbbf1892fb3d75b36938577058
reilley <liqing.fann@gmail.com>
2016-05-29 07:43:02 +0800

    [240. Search a 2D Matrix II][Runtime Error]committed by liqing

---
+++ 240-Search-a-2D-Matrix-II/solution.cs
@@ -0,0 +1,37 @@
+public class Solution {
+    public bool SearchMatrix(int[,] matrix, int target) {
+        int m = matrix.GetLength(0);
+        int n = matrix.GetLength(1);
+        
+        if(m==0 || n==0) return false;
+        return Search(matrix, target, 0,0,m-1, n-1);
+    }
+    
+    private bool Search(int[,] matrix, int target, int up, int left, int down, int right){
+        if(up>down || left>right) return false;
+        if(target<matrix[up,left] || target>matrix[down,right]) return false;
+        if(target==matrix[up,left] || target==matrix[down,right]) return true;
+        
+        int midRow = up + (down-up)/2;
+        int midCol = left + (right-left)/2;
+        int midVal = matrix[midRow, midCol];
+        if(midVal==target) return true;
+        if(target<midVal){
+            bool upleft = Search(matrix, target, up, left, midRow, midCol-1);
+            if(upleft) return true;
+            bool upright = Search(matrix, target, up, midCol, midRow-1, right);
+            if(upright) return true;
+            bool downleft = Search(matrix, target, midRow+1, left, down, midCol-1);
+            if(downleft) return true;
+            
+        } else{
+            bool downright = Search(matrix, target, midRow+1, midCol+1, down, right);
+            if(downright) return true;
+            bool upright = Search(matrix, target, up, midCol+1, midRow+1, right);
+            if(upright) return true;
+            bool downleft = Search(matrix, target, midRow+1, left, down, midCol);
+            if(downleft) return true;
+        }
+        return false;
+    }
+}
