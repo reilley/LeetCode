2f71fcd314c7e38489c8e9977a9ba7b3fa068de2
reilley <liqing.fann@gmail.com>
2016-06-04 15:02:38 +0800

    299. Bulls and Cows

--- 299-Bulls-and-Cows/solution.cs
+++ 299-Bulls-and-Cows/solution.cs
@@ -1,32 +1,18 @@
 public class Solution {
     public string GetHint(string secret, string guess) {
-        var table1 = new Dictionary<int, int>();
-        var table2 = new Dictionary<int, int>();
-        int A = 0, B=0;
+        if(secret.Length==0) return "0A0B";
+        var table = new int[10];
+        int a = 0, b=0;
         for(int i=0; i<secret.Length; i++){
-            int d1 = secret[i];
-            int d2 = guess[i];
-            if(d1==d2) A++;
+            int d1 = secret[i]-'0';
+            int d2 = guess[i]-'0';
+            if(d1==d2) a++;
             else {
-                if(table1.ContainsKey(d2) && table1[d2]>0){
-                    B++;
-                    table1[d2]--;
-                } else if(table2.ContainsKey(d2)) {
-                    table2[d2]++;
-                } else {
-                    table2[d2] = 1;
-                }
-                if(table2.ContainsKey(d1) && table2[d1]>0){
-                    B++;
-                    table2[d1]--;
-                } else if(table1.ContainsKey(d1)) {
-                    table1[d1]++;
-                } else {
-                    table1[d1] = 1;
-                }
+                if(table[d1]++<0)   b++;
+                if(table[d2]-->0) b++;
             }
         }
         
-        return string.Format("{0}A{1}B", A, B);
+        return string.Format("{0}A{1}B", a, b);
     }
 }
