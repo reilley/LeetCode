0a8552b4454003e958cdef1b09c71b1c07ef613d
reilley <liqing.fann@gmail.com>
2016-06-05 08:23:00 +0800

    [220. Contains Duplicate III][Runtime Error]

---
+++ 220-Contains-Duplicate-III/solution.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public bool ContainsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        var dict = new Dictionary<int,int>();
+        if(nums.Length==0 || k<=0 || t<0) return false;
+        for(int i=0; i<nums.Length; i++){
+            if(dict.Any(x => Math.Abs(x.Key-nums[i])<=t && Math.Abs(x.Value-i)<=k)) return true;
+            dict[nums[i]] = i;
+        }
+        return false;
+    }
+}
