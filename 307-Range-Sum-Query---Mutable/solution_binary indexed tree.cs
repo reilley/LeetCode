8f8cf3a5c67853ddfa16ed50b24e498e021ebf0b
reilley <liqing.fann@gmail.com>
2016-06-06 07:30:10 +0800

    [307. Range Sum Query - Mutable][Compile Error] binary indexed tree

---
+++ 307-Range-Sum-Query---Mutable/solution_binary indexed tree.cs
@@ -0,0 +1,47 @@
+public class NumArray {
+    private int[] array;
+    private int[] BITree;
+    public NumArray(int[] nums) {
+        array = new int[nums.Length];
+        BITree = new int[nums.Length+1];
+        for(int i=0; i<nums.Length; i++){
+            array[i] = nums[i];
+            UpdateTree(i, nums[i]);
+        }
+    }
+    
+    private void UpdateTree(int index, int val){
+        index += 1;
+        while(index < BITree.Length){
+            BITree[index] += val;
+            index += index & (-index);
+        }
+    }
+
+    public void Update(int i, int val) {
+        if(i>=array.Length) return;
+        int diff = val - array[i];
+        UpdateTree(i, diff);
+    }
+
+    public int SumRange(int i, int j) {
+        return SumRange(i,j,0,array.Length-1);
+    }
+    
+    private int Sum(int index){
+        index += 1;
+        int sum = 0;
+        while(index > 0){
+            sum += BITree[index];
+            index -= index & (-index);
+        }
+    }
+
+}
+
+
+// Your NumArray object will be instantiated and called as such:
+// NumArray numArray = new NumArray(nums);
+// numArray.SumRange(0, 1);
+// numArray.Update(1, 10);
+// numArray.SumRange(1, 2);
