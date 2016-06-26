eac211f0170179c3b7734c4b5e6331d2b1709191
reilley <liqing.fann@gmail.com>
2016-06-27 02:39:02 +0800

    [316. Remove Duplicate Letters][Accepted]

--- 316-Remove-Duplicate-Letters/solution.cs
+++ 316-Remove-Duplicate-Letters/solution.cs
@@ -2,29 +2,26 @@ public class Solution {
     public string RemoveDuplicateLetters(string s) {
         if(s==null || s.Length==0) return "";
         int[] nums = new int[26];
+        bool[] visited = new bool[26];
         for(int i=0; i<s.Length; i++){
-            nums[i] = -1;
+            nums[s[i]-'a'] ++;
         }
         
-        char[] chars = s.ToCharArray();
-        for(int i=0; i<chars.Length; i++){
-            if(nums[s[i]-'a']>=0){
-                int lastpos = nums[s[i]-'a'];
-                if(s[lastpos]>s[lastpos+1]){
-                    chars[lastpos] = '0';
-                    nums[s[i]-'a'] = i;
-                } else{
-                    chars[i] = '0';
-                }
-            } else {
-                nums[s[i]-'a'] = i;
-            }
-        }
         StringBuilder sb = new StringBuilder();
-        for(int i=0; i<chars.Length; i++){
-            if(chars[i] != '0')
-                sb.Append(chars[i]);
+        for(int i=0; i<s.Length; i++){
+            int index = s[i]-'a';
+            nums[index]--;
+            if(visited[index]) continue;
+            
+            while(sb.Length>0 && s[i]<sb[sb.Length-1] && nums[sb[sb.Length-1]-'a']>0){
+                visited[sb[sb.Length-1]-'a'] = false;
+                sb.Remove(sb.Length-1, 1);
+            }
+            sb.Append(s[i]);
+            visited[index] = true;
         }
+
         return sb.ToString();
     }
+
 }
