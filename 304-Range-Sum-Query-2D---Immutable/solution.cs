8bc8f888765f876825d3f0f030aed4ddbbd26832
reilley <liqing.fann@gmail.com>
2016-07-01 14:50:58 +0800

    [304. Range Sum Query 2D - Immutable][Accepted]

--- 304-Range-Sum-Query-2D---Immutable/solution.cs
+++ 304-Range-Sum-Query-2D---Immutable/solution.cs
@@ -3,14 +3,11 @@ public class NumMatrix {
     private int[,] _sums;
     public NumMatrix(int[,] matrix) {
         
-        _sums = new int[matrix.GetLength(0), matrix.GetLength(1)];
+        _sums = new int[matrix.GetLength(0)+1, matrix.GetLength(1)+1];
 
-        for(int i=0; i<matrix.GetLength(0); i++){
-            _sums[i,0] = matrix[i,0];
-        }
-        for(int i=0; i<matrix.GetLength(0); i++){
-            for(int j=1; j<matrix.GetLength(1); j++){
-                _sums[i,j] = _sums[i, j-1] + matrix[i,j];
+        for(int i=1; i<_sums.GetLength(0); i++){
+            for(int j=1; j<_sums.GetLength(1); j++){
+                _sums[i,j] = _sums[i-1, j] + _sums[i, j-1] - _sums[i-1, j-1] + matrix[i-1,j-1];
             }
         }
         
@@ -18,13 +15,7 @@ public class NumMatrix {
 
     public int SumRegion(int row1, int col1, int row2, int col2) {
         if(row2>=_sums.GetLength(0) || col2>=_sums.GetLength(1)) return 0;
-        int res = 0;
-        for(int i=row1; i<=row2; i++){
-            res += _sums[i, col2];
-            if(col1>0)
-                res -= _sums[i, col1-1];
-        }
-        return res;
+         return _sums[row2+1, col2+1] - _sums[row1, col2+1] - _sums[row2+1, col1] + _sums[row1, col1];
     }
 }
 
