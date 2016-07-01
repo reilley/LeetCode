7ec7f5151b9354d26fcdbfea1c636f0a31c5076e
reilley <liqing.fann@gmail.com>
2016-07-02 02:16:50 +0800

    [287. Find the Duplicate Number][Accepted] simplified

--- 287-Find-the-Duplicate-Number/solution.cs
+++ 287-Find-the-Duplicate-Number/solution.cs
@@ -1,14 +1,12 @@
 public class Solution {
     public int FindDuplicate(int[] nums) {
         if(nums==null || nums.Length<=1) return 0;
-        int slow = 0;
-        int fast = 0;
-        do{
+        int slow = nums[0];
+        int fast = nums[nums[0]];
+        while(slow != fast){
             slow = nums[slow];
             fast = nums[nums[fast]];
-            if(slow!=0 && nums[slow]==nums[0] || fast != 0 && nums[fast]==nums[0]) 
-                return nums[0];
-        }while(slow != fast);
+        };
         
         int res = 0;
         slow = 0;
