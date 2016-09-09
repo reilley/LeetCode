8f5245d4fe23b1c446ca35bc94072fe77596881e
reilley <liqing.fann@gmail.com>
2016-09-08 22:14:54 -0700

    [5. Longest Palindromic Substring][Time Limit Exceeded]

---
+++ 5-Longest-Palindromic-Substring/solution.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() == 0) return "";
+        
+        String ans = "";
+        char[] carr = s.toCharArray();
+        for (int i = 0; i < s.length(); i++) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(carr[i]);
+            int j = i;
+            while (j+1 < carr.length && carr[i] == carr[j+1]) {
+                sb.append(carr[++j]);
+            }
+            
+            helper(carr, i, j, sb);
+            if (sb.length() > ans.length()) {
+                ans = sb.toString();
+            }
+        }
+        
+        return ans;
+    }
+    
+    private void helper(char[] carr, int left, int right, StringBuilder sb) {
+        while (left > 0 && right < carr.length -1 && carr[left-1] == carr[right+1]) {
+            sb.insert(0, carr[--left]);
+            sb.append(carr[++right]);
+        }
+    }
+}
